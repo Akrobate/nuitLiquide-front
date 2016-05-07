1af0f928259ef8beb9dc46fb1dc25e83a745303d
Artiom FEDOROV <artiom@localhost.localdomain>
2016-05-07 03:44:59 +0200

    Ajout wysiwyg sur la page saisie des propositions

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'LocalStorageModule'
+    'LocalStorageModule',
+    'textAngular'
   ])
   .config(['localStorageServiceProvider', function(localStorageServiceProvider){
   localStorageServiceProvider.setPrefix('nuitlq');
